aa4a66882e349e5c2091effa3554a87c804edbd2
WangZhen <wangzhenjjcn@gmail.com>
2018-08-28 16:10:54 +0800

    add

--- source/src/main/java/org/myazure/schedule/ScheduleMananger.java
+++ source/src/main/java/org/myazure/schedule/ScheduleMananger.java
@@ -17,35 +17,37 @@ public class ScheduleMananger {
 
 	@Scheduled(cron = "0/1 * *  * * ? ")
 	protected void secScanner() {
-		System.out.print(".");
-
+		LOG.debug("secScanner");
 	}
 
 	@Scheduled(cron = "0/30 * *  * * ? ")
 	protected void thirtySecScanner() {
-
+		LOG.debug("thirtySecScanner");
 	}
 
 	@Scheduled(cron = "0 0/1 *  * * ? ")
 	protected void minScanner() {
-		System.out.print("\n");
-		System.out.println("Checking Order");
+		LOG.debug("minScanner");
 	}
 
 	@Scheduled(cron = "0 0/10 *  * * ? ")
 	protected void tenMinScanner() {
+		LOG.debug("tenMinScanner");
 	}
 
 	@Scheduled(cron = "0 0/30 *  * * ? ")
 	protected void halfHourScanner() {
+		LOG.debug("halfHourScanner");
 	}
 
 	@Scheduled(cron = "0 0 0/1  * * ? ")
 	protected void hourScanner() {
+		LOG.debug("hourScanner");
 	}
 
 	@Scheduled(cron = "0 0 0 0/1 * ? ")
 	protected void daylyScanner() {
+		LOG.debug("daylyScanner");
 	}
 
 }
